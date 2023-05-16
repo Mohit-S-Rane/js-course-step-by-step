69cfeed15a89485bf547df39afe509529a2a3eeb
Mohit-S-Rane <mohitsrane16@gmail.com>
2023-05-16 17:07:36 +0530

    Function Declarations vs. Expressions

--- src/02-Fundamentals-Part-2/script.js
+++ src/02-Fundamentals-Part-2/script.js
@@ -11,26 +11,43 @@
 // // const interface = 'Audio';
 // // const private = 534;
 
+// ///////////////////////////////////////
+// // Functions
+// function logger() {
+//     console.log('My name is Jonas');
+//   }
+  
+//   // calling / running / invoking function
+//   logger();
+//   logger();
+//   logger();
+  
+//   function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+//   }
+  
+//   const appleJuice = fruitProcessor(5, 0);
+//   console.log(appleJuice);
+  
+//   const appleOrangeJuice = fruitProcessor(2, 4);
+//   console.log(appleOrangeJuice);
+  
+//   const num = Number('23');
+
 ///////////////////////////////////////
-// Functions
-function logger() {
-    console.log('My name is Jonas');
+// Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYeah) {
+    return 2037 - birthYeah;
   }
+  const age1 = calcAge1(1991);
   
-  // calling / running / invoking function
-  logger();
-  logger();
-  logger();
-  
-  function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+  // Function expression
+  const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah;
   }
+  const age2 = calcAge2(1991);
   
-  const appleJuice = fruitProcessor(5, 0);
-  console.log(appleJuice);
-  
-  const appleOrangeJuice = fruitProcessor(2, 4);
-  console.log(appleOrangeJuice);
-  
-  const num = Number('23');
+  console.log(age1, age2);
